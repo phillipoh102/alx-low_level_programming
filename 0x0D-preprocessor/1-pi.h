bdc0944d0a579f1202059a7594fc0d6e76ce8504
phillipoh102 <phillipkitavi555@gmail.com>
2023-04-18 15:02:19 +0300

    update

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,9 @@
+// This is a header file named "pi.h" that defines a macro for the value of pi.
+
+#ifndef PI_H    // Header guard to prevent multiple inclusion of the header file.
+#define PI_H
+
+#define PI 3.14159265359    // Define the value of pi as a macro.
+
+#endif    // End of header guard.
+
