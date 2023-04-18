6bb43810595195f7132ae9ac897f5f034c2b5715
phillipoh102 <phillipkitavi555@gmail.com>
2023-04-18 12:10:05 +0300

    0x0E. C - Structures typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,30 +1,32 @@
-/* 
-This program defines a new type struct dog with the following elements: name, age, and owner.
-*/
+#ifndef DOG_H
+#define DOG_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
-// Define the struct dog
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
-int main() {
-    // Create a new dog
-    struct dog my_dog;
-    
-    // Assign values to the dog's elements
-    my_dog.name = "Fido";
-    my_dog.age = 2.5;
-    my_dog.owner = "John";
-    
-    // Print out the dog's information
-    printf("Name: %s\n", my_dog.name);
-    printf("Age: %.1f\n", my_dog.age);
-    printf("Owner: %s\n", my_dog.owner);
-    
-    return 0;
-}
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
+
