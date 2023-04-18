27aaeb8ad6373884d0611466c18a6291eb727e18
phillipoh102 <phillipkitavi555@gmail.com>
2023-04-18 15:13:06 +0300

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
- *
- * This function prints the name of the source file to the console.
+ * main - prints the name of the file
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    printf("%s\n", __FILE__);    // Prints the name of the source file
-    return 0;    // Indicates successful termination of the program
+	printf("%s\n", __FILE__);
+	return (0);
 }
+
 
